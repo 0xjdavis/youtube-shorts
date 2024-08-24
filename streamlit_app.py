1fc9fb39ab06aa03bc503b54390aee19f150a9d7
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-23 20:59:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,24 @@ st.title("YouTube Shorts Viewer")
 st.sidebar.header("About")
 st.sidebar.write("This app fetches and displays YouTube Shorts videos from a specified channel using the YouTube Data API.")
 
+# Calendly
+st.sidebar.markdown("""
+    <hr />
+    <center>
+    <div style="border-radius:8px;padding:8px;background:#fff";width:100%;">
+    <img src="https://avatars.githubusercontent.com/u/98430977" alt="Oxjdavis" height="100" width="100" border="0" style="border-radius:50%"/>
+    <br />
+    <span style="height:12px;width:12px;background-color:#77e0b5;border-radius:50%;display:inline-block;"></span> <b>I'm available for new projects!</b><br />
+    <a href="https://calendly.com/0xjdavis" target="_blank"><button style="background:#126ff3;color:#fff;border: 1px #126ff3 solid;border-radius:8px;padding:8px 16px;margin:10px 0">Schedule a call</button></a><br />
+    </div>
+    </center>
+    <br />
+""", unsafe_allow_html=True)
+
+# Copyright
+st.sidebar.caption("©️ Copyright 2024 J. Davis")
+
+
 # Input fields
 channel_id = st.text_input("YouTube Channel ID", value="UCLRAP5fUb-OpHEiTryypa0g")
 max_results = st.number_input("Number of videos to fetch (max 500)", min_value=1, max_value=500, value=100)
